22011fcf7f59904c758857a13bf80baed88e4da3
user.email <jobayer.ahmed109@gmail.com>
2023-02-23 12:51:02 +0600

    socketer client app created

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,18 @@
 const app = require("express")();
 const port = process.env.PORT || 5000;
+const chats = require("./data/data");
 
 app.get("/", (req, res) => {
   res.send(`<h1>Socketer app is running</h1>`);
+});
+
+app.get("/api/chats", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chats/:id", (req, res) => {
+  const singleChat = chats.filter((chat) => chat._id === req.params.id);
+  res.send(singleChat);
 });
 
 app.listen(port, () => {
